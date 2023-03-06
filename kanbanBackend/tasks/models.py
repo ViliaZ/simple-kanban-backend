f2566114d56560ce78ed5b573a8ac2dc1809ac11
ViliaZ <vzeisig@gmail.com>
2023-03-06 19:31:57 +0100

    add models

--- kanbanBackend/tasks/models.py
+++ kanbanBackend/tasks/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
-# Create your models here.
+from datetime import datetime
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Task(models.Model):
+    created_at = models.DateTimeField(default=datetime.now)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, default='')
+    assigned_user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    urgency = models.IntegerField(null=True, default=1)
