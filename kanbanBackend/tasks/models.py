e939e5495baf4161f10b76e96e500ca4d2e26889
ViliaZ <vzeisig@gmail.com>
2023-03-07 07:34:39 +0100

    refactor app structure and Task model properties

--- kanbanBackend/tasks/models.py
+++ kanbanBackend/tasks/models.py
@@ -1,13 +1,12 @@
-
 from datetime import datetime
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import User
 
 class Task(models.Model):
     created_at = models.DateTimeField(default=datetime.now)
-    title = models.CharField(max_length=100)
-    description = models.TextField(null=True, default='')
-    assigned_user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, default='empty title')
+    description = models.TextField(null=True, default='empty description')
+    assigned_user = models.ForeignKey(User, related_name='tasks', blank=True, null=True, default=None, on_delete=models.CASCADE)
     urgency = models.IntegerField(null=True, default=1)
+    
+    #category = models.
--- kanbanBackend/tasks/views.py
+++ kanbanBackend/tasks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+#from rest_framework import permissions
+from .serializers import TaskSerializer
+from .models import Task
 
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
+    #permission_classes = [permissions.IsAuthenticated]
 
