8aec51c47475f5dd6fb5e00b52da84426b5d11a8
ViliaZ <vzeisig@gmail.com>
2023-03-07 07:33:42 +0100

    install RESTframework

--- kanbanBackend/kanbanBackend/settings.py
+++ kanbanBackend/kanbanBackend/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login',
-    'tasks'
+    'tasks',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
